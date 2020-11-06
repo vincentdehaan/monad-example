2b0612cacfaec7fb92a1e479940610234cab7ac6
Vincent de Haan <vincentdehaan@gmail.com>
2020-11-06 09:40:14 +0100

    Solved the easy parts

--- src/main/scala/nl/vindh/monadexample/Main.scala
+++ src/main/scala/nl/vindh/monadexample/Main.scala
@@ -3,9 +3,11 @@ package nl.vindh.monadexample
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 
+import scala.concurrent.ExecutionContext
+
 object Main extends App {
-  implicit val system = ActorSystem("monad-system")
-  implicit val executionContext = system.dispatcher
+  implicit val system: ActorSystem = ActorSystem("monad-system")
+  implicit val executionContext: ExecutionContext = system.dispatcher
 
   val frontendRepo = new RandomDeveloperRepository
   val backendRepo = new RandomDeveloperRepository
